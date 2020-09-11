f80ca4536273779bf67e5d46faf77f0ea0dfa651
Carlos Estrada <carlos.estrada@piikaaboo.com>
2020-09-10 23:33:33 -0400

    Created flutter app

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BullsEyeApp());
+
+class BullsEyeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BullsEye',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: GamePage(title: 'BullsEye'),
+    );
+  }
+}
+
+class GamePage extends StatefulWidget {
+  final String title;
+
+  const GamePage({Key key, this.title}) : super(key: key);
+
+  @override
+  _GamePageState createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Hello BullsEye',
+              style: TextStyle(fontWeight: FontWeight.bold, color: Colors.green),
+            ),
+            FlatButton(
+                onPressed: () {},
+                child: Text(
+                  'Hit Me!',
+                  style: TextStyle(color: Colors.blue),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
