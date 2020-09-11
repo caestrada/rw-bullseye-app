06ca1b21c0eba3bbe26d30a97dbbdad36384641b
Carlos Estrada <carlos.estrada@piikaaboo.com>
2020-09-11 16:51:33 -0400

    Add hit me button and show popup

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,8 @@ class GamePage extends StatefulWidget {
 }
 
 class _GamePageState extends State<GamePage> {
+  bool _alertIsVisible = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,7 +37,11 @@ class _GamePageState extends State<GamePage> {
               style: TextStyle(fontWeight: FontWeight.bold, color: Colors.green),
             ),
             FlatButton(
-                onPressed: () {},
+                onPressed: () {
+                  this._alertIsVisible = true;
+                  _showAlert(context);
+                  print('Button pressed!');
+                },
                 child: Text(
                   'Hit Me!',
                   style: TextStyle(color: Colors.blue),
@@ -43,6 +49,31 @@ class _GamePageState extends State<GamePage> {
           ],
         ),
       ),
+    );
+  }
+
+  void _showAlert(BuildContext context) {
+    Widget okButton = FlatButton(
+      child: Text('Awesome!'),
+      onPressed: () {
+        Navigator.of(context).pop();
+        this._alertIsVisible = false;
+        print('Asesome pressed! $_alertIsVisible');
+      },
+    );
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Hello there!'),
+          content: Text('This is my first pop-up.'),
+          actions: [
+            okButton
+          ],
+          elevation: 5.0,
+        );
+      },
     );
   }
 }
