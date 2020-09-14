b4144731aeb1d078387708e60a6928e2d2f80f96
Carlos Estrada <carlos.estrada@piikaaboo.com>
2020-09-13 22:33:09 -0400

    Finish challenge

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class GamePage extends StatefulWidget {
 
 class _GamePageState extends State<GamePage> {
   bool _alertIsVisible = true;
+  bool _whoIsThereAlert = false;
 
   @override
   Widget build(BuildContext context) {
@@ -37,18 +38,52 @@ class _GamePageState extends State<GamePage> {
               style: TextStyle(fontWeight: FontWeight.bold, color: Colors.green),
             ),
             FlatButton(
-                onPressed: () {
-                  this._alertIsVisible = true;
-                  _showAlert(context);
-                  print('Button pressed!');
-                },
-                child: Text(
-                  'Hit Me!',
-                  style: TextStyle(color: Colors.blue),
-                ))
+              onPressed: () {
+                this._alertIsVisible = true;
+                _showAlert(context);
+                print('Button pressed!');
+              },
+              child: Text(
+                'Hit Me!',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+            FlatButton(
+              onPressed: () {
+                this._whoIsThereAlert = true;
+                this._showWhosThere(context);
+              },
+              child: Text(
+                'Knock Knock!',
+                style: TextStyle(color: Colors.blue),
+              ),
+            )
           ],
         ),
       ),
+    );
+  }
+
+  void _showWhosThere(BuildContext context) {
+    Widget okButton = FlatButton(
+      child: Text('Owls say who?'),
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+    );
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Who's there?"),
+          content: Text('Owls say'),
+          actions: [
+            okButton
+          ],
+          elevation: 5.0,
+        );
+      },
     );
   }
 
